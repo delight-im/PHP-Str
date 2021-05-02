7f2c86cf28fbcc4ae0aff361e6682dd41f3be0a4
Marco <marco@delight.im>
2021-05-02 21:35:11 +0200

    Use explicit namespaces in tests

--- tests/index.php
+++ tests/index.php
@@ -7,15 +7,15 @@
  */
 
 // enable error reporting
-error_reporting(E_ALL);
-ini_set('display_errors', 'stdout');
+\error_reporting(E_ALL);
+\ini_set('display_errors', 'stdout');
 
 // enable assertions
-ini_set('assert.active', 1);
-ini_set('zend.assertions', 1);
-ini_set('assert.exception', 1);
+\ini_set('assert.active', 1);
+\ini_set('zend.assertions', 1);
+\ini_set('assert.exception', 1);
 
-header('Content-type: text/html; charset=utf-8');
+\header('Content-type: text/html; charset=utf-8');
 
 require __DIR__.'/../vendor/autoload.php';
 
