b622c85f6b0cee82e132adfabcb9e94453c0e273
Marco <marco@delight.im>
2021-10-20 11:03:59 +0200

    Re-implement 'endsWithIgnoreCase' to be faster
    The new implementation is more CPU-efficient while being equally
    memory-efficient. The exact performance, of course, depends on the
    length and nature of the haystack and the needle. Anyway, this is
    a micro-optimization, as the operation is generally very fast and
    running at (single-digit) millions of operations per second on a
    single modern CPU core.

--- src/Str.php
+++ src/Str.php
@@ -141,9 +141,9 @@ final class Str implements \Countable {
 	 * @return bool whether the supplied other string can be found at the end of this string
 	 */
 	public function endsWithIgnoreCase($suffix) {
-		$other = new Str($suffix, $this->charset);
+		$suffixLength = \strlen($suffix);
 
-		return \mb_strripos($this->rawString, $suffix, 0, $this->charset) === ($this->length() - $other->length());
+		return $suffix !== '' && \substr_compare($this->rawString, $suffix, -$suffixLength, $suffixLength, true) === 0;
 	}
 
 	/**
