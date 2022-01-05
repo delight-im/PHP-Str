55631e216d355ad32424de30a3a26536ad3a4cca
Marco <marco@delight.im>
2022-01-05 17:02:59 +0100

    Add tests for method 'isAscii'

--- tests/index.php
+++ tests/index.php
@@ -213,6 +213,12 @@ $japaneseEucJpStr = \mb_convert_encoding($japaneseUtf8Str, 'EUC-JP', 'UTF-8');
 (Str::from('abc')->isEmpty() === false) or \fail(__LINE__);
 ($testStrObj->isEmpty() === false) or \fail(__LINE__);
 
+(Str::from('')->isAscii() === true) or \fail(__LINE__);
+(Str::from("\t\n")->isAscii() === true) or \fail(__LINE__);
+(Str::from(' !Aa}~')->isAscii() === true) or \fail(__LINE__);
+(Str::from('W☺rld')->isAscii() === false) or \fail(__LINE__);
+($testStrObj->isAscii() === false) or \fail(__LINE__);
+
 ((string) $testStrObj->toLowerCase() === 'hello hello w☺rld w☺rld') or \fail(__LINE__);
 ($testStrObj->isLowerCase() === false) or \fail(__LINE__);
 ($testStrObj->toLowerCase()->isLowerCase() === true) or \fail(__LINE__);
@@ -643,6 +649,11 @@ $c = $b->codePointAt(6);
 
 $b = Str::from(" § World 'world' \u{1F30D} & \u{1F30E} & \u{1F30F} 'world' world\rA!\r\nB!\nC! § ");
 $c = $b->isEmpty();
+((string) $a === (string) $b && \gettype($a) === \gettype($b)) or \fail(__LINE__);
+(\gettype($b) !== \gettype($c)) or \fail(__LINE__);
+
+$b = Str::from(" § World 'world' \u{1F30D} & \u{1F30E} & \u{1F30F} 'world' world\rA!\r\nB!\nC! § ");
+$c = $b->isAscii();
 ((string) $a === (string) $b && \gettype($a) === \gettype($b)) or \fail(__LINE__);
 (\gettype($b) !== \gettype($c)) or \fail(__LINE__);
 
