80feb64739470ef7cf2ff38283c67297a422dfd0
Marco <marco@delight.im>
2022-01-10 10:26:24 +0100

    Implement byte and code point variants of 'replaceFirst'

--- src/Str.php
+++ src/Str.php
@@ -901,7 +901,7 @@ final class Str implements \Countable {
 	}
 
 	/**
-	 * Replaces the first occurrence of the specified search string with the given replacement
+	 * Replaces the first occurrence of the specified search string with the given replacement based on bytes
 	 *
 	 * This operation is case-sensitive
 	 *
@@ -911,8 +911,34 @@ final class Str implements \Countable {
 	 * @param string $replaceWith the string to use as the replacement (optional)
 	 * @return static a new instance of this class
 	 */
-	public function replaceFirst($searchFor, $replaceWith = null) {
+	public function replaceFirstBytes($searchFor, $replaceWith = null) {
+		return $this->replaceOneInternal(true, false, 'strpos', $searchFor, $replaceWith);
+	}
+
+	/**
+	 * Replaces the first occurrence of the specified search string with the given replacement based on code points
+	 *
+	 * This operation is case-sensitive
+	 *
+	 * The empty string is not considered to be a part of any other string
+	 *
+	 * @param string $searchFor the string to search for
+	 * @param string $replaceWith the string to use as the replacement (optional)
+	 * @return static a new instance of this class
+	 */
+	public function replaceFirstCodePoints($searchFor, $replaceWith = null) {
 		return $this->replaceOneInternal(false, true, 'mb_strpos', $searchFor, $replaceWith);
+	}
+
+	/**
+	 * Alias of `replaceFirstCodePoints`
+	 *
+	 * @param string $searchFor
+	 * @param string $replaceWith
+	 * @return static
+	 */
+	public function replaceFirst($searchFor, $replaceWith = null) {
+		return $this->replaceFirstCodePoints($searchFor, $replaceWith);
 	}
 
 	/**
